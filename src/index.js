536505150cb15765387d18b8e78a9929f01902fb
lykmapipo <lallyelias87@gmail.com>
2019-08-30 16:10:02 +0300

    refactor: improve main exports

--- src/index.js
+++ src/index.js
@@ -9,24 +9,28 @@
  * @version 0.1.0
  * @example
  *
- * const { app } = require('@codetanzania/majifix-account');
- *
- * ...
- *
- * app.start();
+ * const { Account, start } = require('@codetanzania/majifix-account');
+ * start(error => { ... });
  *
  */
-
-/* dependencies */
-import { pkg } from '@lykmapipo/common';
 import _ from 'lodash';
+import { pkg } from '@lykmapipo/common';
 import { apiVersion as httpApiVersion } from '@lykmapipo/env';
+import { start } from '@lykmapipo/express-common';
 import Account from './account.model';
 import accountRouter from './account.http.router';
 
-/* declarations */
+/**
+ * @name info
+ * @description package information
+ * @type {object}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 const info = pkg(
-  '../package.json',
+  `${__dirname}/package.json`,
   'name',
   'description',
   'version',
@@ -40,28 +44,67 @@ const info = pkg(
 
 /* export function */
 const account = integration => {
-  // ensure integration integration
-
+  // ensure integration
   if (integration) {
     const { fetchAccount } = integration;
     if (_.isFunction(fetchAccount)) {
       Account.fetchAccount = fetchAccount;
     }
   }
-
   return account;
 };
 
-// export info
+/**
+ * @name info
+ * @description package information
+ * @type {object}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 account.info = info;
 
-// export apiVersion
+/**
+ * @name apiVersion
+ * @description http router api version
+ * @type {string}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 account.apiVersion = httpApiVersion();
 
-// export router
+/**
+ * @name accountRouter
+ * @description account http router
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 account.accountRouter = accountRouter;
 
-// export Account
+/**
+ * @name Account
+ * @description Account model
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 account.Account = Account;
+
+/**
+ * @function start
+ * @name start
+ * @description start http server
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 0.1.0
+ * @version 0.1.0
+ */
+account.start = start;
 
 export default account;
